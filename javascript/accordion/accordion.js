c97aaa62a80df1d5049bf68a394c023c76dbe578
fathergoose <al@ilseman.com>
2016-01-26 10:28:46 -0600

    actAccordion is an object

--- javascript/accordion/accordion.js
+++ javascript/accordion/accordion.js
@@ -1,1 +1,4 @@
-var hello = true;
+var hello = true; // eslint-disable-line
+
+var actAccordion = document.getElementById('act-sections');
+console.log(actAccordion);
--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
+/* eslint-disable */
+$(document).ready(function() {
   module("Accordion tests");
 
   test("example test", function() {
     ok(true);
   });
 
-  test("hello exists", function(){
+  test("hello exists", function() {
     ok(hello, "hello exists");
   });
 
-});
+  test("actAccordion is an object", function() {
+    equal("object", typeof(actAccordion), "accordion is an object")
+  })
+
+}
+);
