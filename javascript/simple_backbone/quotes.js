1e1e854a15c3f9931ff3c1007763b11e71e61f18
fathergoose <al@ilseman.com>
2016-01-26 19:04:26 -0600

    grabbing quotes

--- javascript/simple_backbone/quotes.js
+++ javascript/simple_backbone/quotes.js
@@ -1,7 +1,14 @@
-var  test = Backbone.Model.extend({
+/* global Backbone */
+var test = Backbone.Model.extend({ // eslint-disable-line
   initialize: function() {
-    alert("hey hey hey");
+    alert('hey hey hey'); // eslint-disable-line
   }
 });
 
-// https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json
+var Quotes = Backbone.Collection.extend({
+  url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json'
+});
+
+var quotes = new Quotes();
+quotes.fetch({
+});
