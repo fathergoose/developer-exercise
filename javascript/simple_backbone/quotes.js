65c2a28ecdcaa1bd3d55539faf1d3ccfcb5c9c86
fathergoose <al@ilseman.com>
2016-01-26 18:34:57 -0600

    got backbone to do something

---
+++ javascript/simple_backbone/quotes.js
@@ -0,0 +1,7 @@
+var  test = Backbone.Model.extend({
+  initialize: function() {
+    alert("hey hey hey");
+  }
+});
+
+// https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json
