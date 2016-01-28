4685ae24f8c593bfe41e13dafc5ad65e007f651b
fathergoose <al@ilseman.com>
2016-01-28 13:30:18 -0600

    Understanding basic backbone design, now need to get onto the page

--- javascript/simple_backbone/quotes.js
+++ javascript/simple_backbone/quotes.js
@@ -23,20 +23,25 @@
     Used to create own model classes w/ backbone funcationality
 
 */
-var test = Backbone.Model.extend({ // eslint-disable-line
+var Quote = Backbone.Model.extend({ // eslint-disable-line
   initialize: function() {
-    alert('hey hey hey'); // eslint-disable-line
+    console.log('new Quote', this); // eslint-disable-line
+  },
+  defaults: {
+    context: '',
+    quote: '',
+    source: '',
+    theme: '',
   }
 });
 
 var Quotes = Backbone.Collection.extend({
-  model: 'quoteModel',
-  url: 'https://gist.githubusercontent.com/anonymous/\
-  8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/\
-  quotes.json',
-//  parse: function(data) {
-//  return data.books; // parse can help clean up a messy API
-//  }
+  model: Quote,
+//  url: 'quotes.json'
+  url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json',
+  // parse: function(data) {
+  // return data.responseJSON; // parse can help clean up a messy API
+  // }
 });
 
 // Fetching should happen in the view rendering function
@@ -54,9 +59,25 @@ var Quotes = Backbone.Collection.extend({
 //    These work by setting var somevar = _.template();
 //  Views need el property in initialization (JS) and somewhere on the page (HTML) so they can bind events from the DOM
 //    Someone just refered to el as a "landing place" for template content
+//  I want to have a render() called on initialization of the view objects
+//    have render() load the template into the el property
 
 var QuoteView = Backbone.View.extend({
+  el:'#quote-container',
   initialize: function() {
-    alert("view initialized");
+    this.collection = new Quotes;
+    this.render(); // I may have written render directly into initialize, but this seems the recommended technique
+    console.log("view initialized", this);
+  },
+  render: function() {
+    var that = this;
+    var promise = this.collection.fetch();
+    promise.done(function() {
+      var quoteTemplate = _.template( $("#quote-template").html() ); // use underscore to make a template out of some html gotten using jQuery
+      var quoteHTML = quoteTemplate({quotes : this.collection });  // feed that template our collection
+      this.$el.html( quoteTemplate ); // this view's el-zone needs html... let's use quoteTemplate
+    });
   }
-})
+});
+
+var quoteView = new QuoteView;
