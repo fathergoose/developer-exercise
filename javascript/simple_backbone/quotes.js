e6b25d3c6b20d5146b5d2b7e1c83000126e7beed
fathergoose <al@ilseman.com>
2016-01-27 18:57:42 -0600

    view initialized when called from console and notes on views/templates

--- javascript/simple_backbone/quotes.js
+++ javascript/simple_backbone/quotes.js
@@ -3,8 +3,11 @@
 
 **Backbone JS notes**
   Uses models and views to provide organization
-    Models: Emit events to views, hold business logic, hold data
-    Views: Listen for model changes, interact with user, sends captured data to model
+    Models:
+      Emit events to views, hold business logic, hold data
+    Views:
+      Listen for model changes, interact with user, sends captured data to model
+      Reflect what the model data 'looks' like
   Collections
     used to get multiple model objects
     ex. some_collection.fetch() ~= GET /quotes/
@@ -19,7 +22,6 @@
   Extend:
     Used to create own model classes w/ backbone funcationality
 
-
 */
 var test = Backbone.Model.extend({ // eslint-disable-line
   initialize: function() {
@@ -28,7 +30,7 @@ var test = Backbone.Model.extend({ // eslint-disable-line
 });
 
 var Quotes = Backbone.Collection.extend({
-  model: quoteModel,
+  model: 'quoteModel',
   url: 'https://gist.githubusercontent.com/anonymous/\
   8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/\
   quotes.json',
@@ -39,6 +41,22 @@ var Quotes = Backbone.Collection.extend({
 
 // Fetching should happen in the view rendering function
 // Something like var quoteView = Backbone.View.extend(el:'#somediv', {},{},)
-var quotes = new Quotes();
-quotes.fetch({
-});
+// Fetching returns a promise and can be used with promise.done(function(){do once promise is fufilled})
+// .done() is a jQuery method for deferred objects allong with deferred.always() and .fail()
+// var quotes = new Quotes();
+// quotes.fetch({
+// });
+
+// Views
+
+//Templates with views
+//  Apparently many options, but _.template() seems to be std.
+//    These work by setting var somevar = _.template();
+//  Views need el property in initialization (JS) and somewhere on the page (HTML) so they can bind events from the DOM
+//    Someone just refered to el as a "landing place" for template content
+
+var QuoteView = Backbone.View.extend({
+  initialize: function() {
+    alert("view initialized");
+  }
+})
