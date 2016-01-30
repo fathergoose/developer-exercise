8bbf10d63875ac590581b51daf168c752e130105
fathergoose <al@ilseman.com>
2016-01-29 22:52:18 -0600

    refactored search base into constant

--- ruby/youtube/youtube_search.rb
+++ ruby/youtube/youtube_search.rb
@@ -3,12 +3,13 @@ require "uri"
 require "json"
 
 API_KEY = ENV["YOUTUBE_API_KEY"]
+YOUTUBE_V3_SEARCH = "https://www.googleapis.com/youtube/v3/search"
 
 class YoutubeSearch
   attr_reader :search_data, :results
   
   def initialize(query)
-    uri = URI.parse("https://www.googleapis.com/youtube/v3/search?part=snippet&q=boating&key=#{API_KEY}")
+    uri = URI.parse("#{YOUTUBE_V3_SEARCH}?part=snippet&q=#{query}&key=#{API_KEY}")
     response_hash = JSON.parse(Net::HTTP.get(uri))
     @search_data = response_hash["items"]
     @results = []
