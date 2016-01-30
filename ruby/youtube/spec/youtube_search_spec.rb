07d6fb9a64f566366330b37820d52ebc666c76df
fathergoose <al@ilseman.com>
2016-01-29 21:34:16 -0600

    refactored so YoutubeSearc.new returns search object(results)

---
+++ ruby/youtube/spec/youtube_search_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe YoutubeSearch do
+
+  describe ".new" do
+
+    it "takes a string and returns a search object" do
+      @boat_search = YoutubeSearch.new("boats")
+      expect(@boat_search).to be_instance_of(YoutubeSearch)
+    end
+
+  end
+
+  describe "#results" do
+
+    it "is an array" do
+      @boat_search = YoutubeSearch.new("boats")
+      expect(@boat_search.results).to be_instance_of(Array)
+    end
+
+  end
+  
+end
---
+++ ruby/youtube/youtube_search.rb
@@ -0,0 +1,8 @@
+class YoutubeSearch
+  attr_reader :results
+  
+  def initialize(query)
+    @results = []
+  end
+
+end
