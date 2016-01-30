a19af6b4c0dbb95186d868f5930170af5e4be8a7
fathergoose <al@ilseman.com>
2016-01-29 20:38:36 -0600

    testing Search.youtube()

--- ruby/youtube/spec/search_spec.rb
+++ ruby/youtube/spec/search_spec.rb
@@ -1,4 +1,12 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Search do
+
+  describe ".youtube" do
+    it "takes a string and returns a search object" do
+      @boat_search = Search.youtube("boats")
+      expect(@boat_search).to be_instance_of(Search)
+    end
+  end
+  
 end
