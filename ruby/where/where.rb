6fb20202c90417e6fb3cc593f4f9335311ace81a
fathergoose <al@ilseman.com>
2016-01-30 14:38:13 -0600

    passing exact match and multiple criteria tests

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,5 +1,13 @@
 class Array
   def where(what)
-
+    results = []
+    self.each do |quoteHash|
+      what.each do |key,value|
+        if quoteHash[key] == what[key]
+          results << quoteHash
+        end
+      end
+    end
+    return results
   end
 end
