3d5b06da82ddc32152c087dc69008d5a38e6d5f0
fathergoose <al@ilseman.com>
2016-01-29 23:00:01 -0600

    refactored tests

--- ruby/youtube/spec/youtube_search_spec.rb
+++ ruby/youtube/spec/youtube_search_spec.rb
@@ -2,15 +2,17 @@ require "spec_helper"
 
 describe YoutubeSearch do
 
+  before :each do
+    @boat_search = YoutubeSearch.new("boats")
+  end
+
   describe ".new" do
 
     it "takes a string and returns a search object" do
-      @boat_search = YoutubeSearch.new("boats")
       expect(@boat_search).to be_instance_of(YoutubeSearch)
     end
 
-    it "should search youtube and return search responses" do
-      @boat_search = YoutubeSearch.new("boats")
+    it "should return search data as an array of hashes" do
       expect(@boat_search.search_data).to be_instance_of(Array)
     end
 
@@ -19,9 +21,12 @@ describe YoutubeSearch do
   describe "#results" do
 
     it "is an array" do
-      @boat_search = YoutubeSearch.new("boats")
       expect(@boat_search.results).to be_instance_of(Array)
     end
+
+    it "should contain Video objects" do
+    end
+
 
   end
   
