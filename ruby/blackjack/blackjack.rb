401a78ed50a6cfa8c024648771de1e0310ffc6e0
fathergoose <al@ilseman.com>
2016-01-31 11:41:01 -0600

    got my players test to fail

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -58,7 +58,7 @@ class CardTest < Test::Unit::TestCase
   def setup
     @card = Card.new(:hearts, :ten, 10)
   end
-  
+
   def test_card_suite_is_correct
     assert_equal @card.suite, :hearts
   end
@@ -75,11 +75,11 @@ class DeckTest < Test::Unit::TestCase
   def setup
     @deck = Deck.new
   end
-  
+
   def test_new_deck_has_52_playable_cards
     assert_equal @deck.playable_cards.size, 52
   end
-  
+
   def test_dealt_card_should_not_be_included_in_playable_cards
     card = @deck.deal_card
     assert(!@deck.playable_cards.include?(card))
@@ -88,5 +88,15 @@ class DeckTest < Test::Unit::TestCase
   def test_shuffled_deck_has_52_playable_cards
     @deck.shuffle
     assert_equal @deck.playable_cards.size, 52
+  end
+end
+
+class GameTest < Test::Unit::TestCase
+  def setup
+    @game = Game.new
+  end
+
+  def test_game_should_have_player_and_dealer
+    assert_equal @game.players.length, 2
   end
 end
